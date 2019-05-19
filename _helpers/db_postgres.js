35808d398c9533537356a1d6de018b8850d3c870
jloomisVCE <jtloomis@gmail.com>
2019-05-19 10:42:44 -0400

    server port argument.

--- _helpers/db_postgres.js
+++ _helpers/db_postgres.js
@@ -2,13 +2,7 @@ const config = require('config.json');
 
 const { Pool } = require('pg'); //a Postgres Connection Pool, not to be confused with a Vernal Pool
 
-const connPool = new Pool({
-  user: 'vpatlas',
-  host: 'localhost',
-  database: 'vpatlas',
-  password: 'EatArugula',
-  port: 5433,
-});
+const connPool = new Pool(config.pg);
 
 /*
 const connPool = new Pool({
--- server.js
+++ server.js
@@ -6,6 +6,37 @@ const bodyParser = require('body-parser');
 const jwt = require('_helpers/jwt');
 const errorHandler = require('_helpers/error-handler');
 
+// Command-Line Arguments Processing
+// This MUST occur before the requires() below. Note that appVersion, which corresponds to the
+// server-specific API version (the default of which is defined in config.js), CAN be overridden
+// via the command line.
+var noauth = 0;
+var http = 0;
+var argPort = 0;
+
+console.log(process.argv);
+
+for (var i=0; i<process.argv.length; i++) {
+	switch(process.argv[i]) {
+		case "noauth":
+			noauth = 1;
+			break;
+		case "http":
+			http=1;
+			break;
+		case "https":
+			http=0;
+			break;
+		case "http2":
+			http=2;
+			break;
+        case "port":
+            argPort=process.argv[++i];
+            console.log(`argPort: ${argPort}`);
+            break;
+	}
+}
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
@@ -22,7 +53,8 @@ app.use('/pools/mapped', require('./vpMapped/vpMapped.routes'));
 app.use(errorHandler);
 
 // start server
-const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 4000;
-const server = app.listen(port, function () {
-    console.log('Server listening on port ' + port);
+var srvPort = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 4000;
+if (argPort) srvPort = argPort;
+const server = app.listen(srvPort, function () {
+    console.log('Server listening on port ' + srvPort);
 });
