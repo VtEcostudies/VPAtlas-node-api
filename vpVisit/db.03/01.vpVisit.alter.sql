b76744d2a1f4913ff4d43f76cf48c0c90339360a
jloomisVCE <jtloomis@gmail.com>
2019-07-13 10:30:40 -0400

    fix errors in vpvisit table.

--- vpVisit/db.03/01.vpVisit.alter.sql
+++ vpVisit/db.03/01.vpVisit.alter.sql
@@ -1,3 +1,5 @@
 ALTER TABLE vpvisit ALTER COLUMN "visitDate" TYPE DATE USING "visitDate"::date;
 ALTER TABLE vpvisit ALTER COLUMN "visitPoolId" SET NOT NULL;
 ALTER TABLE vpvisit RENAME "visitTown" TO "visitTownName";
+ALTER TABLE vpvisit ADD COLUMN "visitLocationUncertainty" TEXT;
+ALTER TABLE vpvisit RENAME COLUMN "visitCoordsource" TO "visitCoordSource";
