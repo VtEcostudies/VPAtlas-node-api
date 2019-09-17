0f13b96b8c96f94b49fa960db174f31a34b8eb36
jloomisVCE <jtloomis@gmail.com>
2019-09-16 23:29:15 -0400

    observer and photos

--- vpMapped/db.06/01.vpmapped.alter.sql
+++ vpMapped/db.06/01.vpmapped.alter.sql
@@ -1,5 +1,5 @@
---add a column to record the actual visiting person called 'visitObserverUserName'
+--add a column to record the actual person who mapped the pool in the field 'mappedObserverUserName'
 
 --Initially, don't require this field. Leave it NULL for existing rows.
 --After adding to table and adding appropriate values for existing data, make it required.
-ALTER TABLE vpvisit ADD COLUMN "visitObserverUserName" TEXT;
+ALTER TABLE vpmapped ADD COLUMN "mappedObserverUserName" TEXT;
