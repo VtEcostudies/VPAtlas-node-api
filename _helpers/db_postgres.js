9bd87ffec3036b763a82b46165165ef5803dc7df
jloomisVCE <jtloomis@gmail.com>
2019-10-02 14:14:23 -0400

    fix date parsing bug introduce by pg_postgres default behavior.

--- _helpers/db_postgres.js
+++ _helpers/db_postgres.js
@@ -1,6 +1,20 @@
 const config = require('config.json');
 const { Pool } = require('pg'); //a Postgres Connection Pool, not to be confused with a Vernal Pool
 const connPool = new Pool(config.pg);
+var types = require('pg').types
+//type date OID=1082
+//type timestamp OID=1114
+
+/*
+simply by adding a function to return the raw value, we
+override the pg_postgres default behavior, which  mucks
+date-only  values by ‘correcting’ for local TZ.var parseDate = function(val) {
+*/
+   console.log('db_postgres.parseDate', val);
+   return val;
+}
+
+types.setTypeParser(1082, parseDate);
 
 /*
 NOTES:
