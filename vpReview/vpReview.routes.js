2da0973c6cbcf3dc8e28feb5c4acb275328f0bca
jloomisVCE <jtloomis@gmail.com>
2020-03-23 21:38:47 -0400

    mostly working vpreview

---
+++ vpReview/vpReview.routes.js
@@ -0,0 +1,73 @@
+﻿const express = require('express');
+const router = express.Router();
+const service = require('./vpReview.service');
+
+// routes
+router.get('/columns', getColumns);
+router.get('/count', getCount);
+router.get('/', getAll);
+router.get('/:id', getById);
+router.post('/', create);
+router.put('/:id', update);
+router.delete('/:id', _delete);
+
+module.exports = router;
+
+function getColumns(req, res, next) {
+    service.getColumns()
+        .then(columns => res.json(columns))
+        .catch(err => next(err));
+}
+
+function getCount(req, res, next) {
+    service.getCount(req.query)
+        .then(items => res.json(items))
+        .catch(err => next(err));
+}
+
+function getAll(req, res, next) {
+    service.getAll(req.query)
+        .then(items => res.json(items))
+        .catch(err => next(err));
+}
+
+function getById(req, res, next) {
+    service.getById(req.params.id)
+        .then(item => item ? res.json(item) : res.sendStatus(404))
+        .catch(err => next(err));
+}
+
+function create(req, res, next) {
+    console.log(`create req.body:`);
+    console.dir(req.body);
+    service.create(req.body)
+        .then((item) => res.json(item))
+        .catch(err => {
+            console.log('vpReview.routes.create | error: ' , err);
+            if (err.code == 23505 && err.constraint == 'vpreview_pkey') {
+                err.name = 'UniquenessConstraintViolation';
+                err.message = `Review ID '${req.body.reviewId}' is already taken. Please choose a different Review ID.`;
+            }
+            next(err);
+        });
+}
+
+function update(req, res, next) {
+    console.log('vpReview.routes.update', req.body);
+    service.update(req.params.id, req.body)
+        .then((item) => res.json(item))
+        .catch(err => {
+            console.log('vpReview.routes.update | error: ' , err);
+            if (err.code == 23505 && err.constraint == 'vpreview_pkey') {
+                err.name = 'UniquenessConstraintViolation';
+                err.message = `Review ID '${req.body.reviewId}' is already taken. Please choose a different Review ID.`;
+            }
+            next(err);
+        });
+}
+
+function _delete(req, res, next) {
+    service.delete(req.params.id)
+        .then(() => res.json({}))
+        .catch(err => next(err));
+}
---
+++ vpReview/vpReview.service.js
@@ -0,0 +1,117 @@
+﻿const db = require('_helpers/db_postgres');
+const query = db.query;
+const pgUtil = require('_helpers/db_pg_util');
+var staticColumns = [];
+
+module.exports = {
+    getColumns,
+    getCount,
+    getAll,
+    getById,
+    create,
+    update,
+    delete: _delete
+};
+
+//file scope list of vpreview table columns retrieved on app startup (see 'getColumns()' below)
+pgUtil.getColumns("vpreview", staticColumns) //run it once on init: to create the array here. also diplays on console.
+    .then(res => {
+        staticColumns.push(`vptown."townName"`); //Add this for town filter query
+        staticColumns.push(`visittown."townName"`); //Add this for town filter query
+        staticColumns.push(`mappedtown."townName"`); //Add this for town filter query
+        return res;
+    })
+    .catch(err => {
+        console.log(`vpReview.service.pg.pgUtil.getColumns | error: `, err.message);
+    });
+
+function getColumns() {
+    console.log(`vpReview.service.pg.getColumns | staticColumns:`, staticColumns);
+    return staticColumns;
+}
+
+async function getCount(body={}) {
+    const where = pgUtil.whereClause(body, staticColumns);
+    const text = `select count(*) from vpreview ${where.text};`;
+    console.log(text, where.values);
+    return await query(text, where.values);
+}
+
+/*
+NOTE:
+*/
+async function getAll(params={}) {
+    var orderClause = 'order by "reviewId"';
+    if (params.orderBy) {
+        var col = params.orderBy.split("|")[0];
+        var dir = params.orderBy.split("|")[1]; dir = dir ? dir : '';
+        orderClause = `order by "${col}" ${dir}`;
+    }
+    const where = pgUtil.whereClause(params, staticColumns);
+    const text = `
+        SELECT
+        (SELECT COUNT(*) FROM vpreview INNER JOIN vpvisit ON vpvisit."visitId"=vpreview."reviewVisitId" ${where.text}) AS count,
+        to_json(mappedtown) AS "mappedTown",
+        to_json(visittown) AS "visitTown",
+        vpreview.*,
+        vpreview."updatedAt" AS "reviewUpdatedAt",
+        vpreview."createdAt" AS "reviewCreatedAt",
+        vpvisit.*,
+        vpvisit."updatedAt" AS "visitUpdatedAt",
+        vpvisit."createdAt" AS "visitCreatedAt",
+        vpmapped.*,
+        vpmapped."updatedAt" AS "mappedUpdatedAt",
+        vpmapped."createdAt" AS "mappedCreatedAt"
+        FROM vpreview
+        INNER JOIN vpvisit ON vpvisit."visitId"=vpreview."reviewVisitId"
+        INNER JOIN vpmapped ON vpmapped."mappedPoolId"=vpreview."reviewPoolId"
+        LEFT JOIN vptown AS mappedtown ON vpmapped."mappedTownId"=mappedtown."townId"
+        LEFT JOIN vptown AS visittown ON vpvisit."visitTownId"=visittown."townId"
+        ${where.text} ${orderClause};`;
+    console.log(text, where.values);
+    return await query(text, where.values);
+}
+
+async function getById(id) {
+    const text = `
+        SELECT
+        to_json(mappedtown) AS "mappedTown",
+        to_json(visittown) AS "visitTown",
+        vpreview.*,
+        vpreview."updatedAt" AS "reviewUpdatedAt",
+        vpreview."createdAt" AS "reviewCreatedAt",
+        vpvisit.*,
+        vpvisit."updatedAt" AS "visitUpdatedAt",
+        vpvisit."createdAt" AS "visitCreatedAt",
+        vpmapped.*,
+        vpmapped."updatedAt" AS "mappedUpdatedAt",
+        vpmapped."createdAt" AS "mappedCreatedAt"
+        FROM vpreview
+        INNER JOIN vpvisit ON vpvisit."visitId"=vpreview."reviewVisitId"
+        INNER JOIN vpmapped ON vpmapped."mappedPoolId"=vpreview."reviewPoolId"
+        LEFT JOIN vptown AS mappedtown ON vpmapped."mappedTownId"=mappedtown."townId"
+        LEFT JOIN vptown AS visittown ON vpvisit."visitTownId"=visittown."townId"
+        WHERE "reviewId"=$1;`;
+    return await query(text, [id])
+}
+
+async function create(body) {
+    var queryColumns = pgUtil.parseColumns(body, 1, [], staticColumns);
+    text = `insert into vpreview (${queryColumns.named}) values (${queryColumns.numbered}) returning "reviewId"`;
+    console.log(text, queryColumns.values);
+    var res = await query(text, queryColumns.values);
+    console.log('vpReview.service.create | returning: ', res);
+    return res;
+}
+
+async function update(id, body) {
+    console.log(`vpReview.service.update | before pgUtil.parseColumns`, staticColumns);
+    var queryColumns = pgUtil.parseColumns(body, 2, [id], staticColumns);
+    text = `update vpreview set (${queryColumns.named}) = (${queryColumns.numbered}) where "reviewId"=$1 returning "reviewId"`;
+    console.log(text, queryColumns.values);
+    return await query(text, queryColumns.values);
+}
+
+async function _delete(id) {
+    return await query(`delete from vpreview where "reviewId"=$1;`, [id]);
+}
