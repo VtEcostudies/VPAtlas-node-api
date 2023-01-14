b5295e396e9de8b9372a291952eed5659de411b5
jloomisVCE <jloomis@vtecostudies.org>
2023-01-14 19:37:40 +0000

    add watch dirs to pm2 config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,3 +1,7 @@
+/*
+ How to configure watch is not obvious. See this post:
+ https://stackoverflow.com/questions/57791439/how-to-get-pm2-process-to-watch-after-it-has-been-stopped-restarted
+*/
 module.exports = {
   apps : [{
     script : "./server.js",
@@ -7,17 +11,13 @@ module.exports = {
     watch: true,  // auto restart app on change
     ignore_watch: ["node_modules"],
     watch_delay: 3000,
+    watch_options: {"usePolling": true},
 /*
     wait_ready: true, // wait for app to send process.send('ready')
     listen_timeout: 10000, //timeout to wait for the ready signal, otherwise... do what?
 */
-    // Removing the default env means a no-arg call ('pm2 start')
-    // will try to detect server context from os hostname
 /*
-    env: {
-       NODE_ENV: "prod",
-       watch: ["./server.js", "/etc/letsencrypt/live"]
-    },
+ Note: Removing the default env means a no-arg call ('pm2 start') will try to detect server context from os hostname
 */
     env_dev: {
        NODE_ENV: "dev-local",
@@ -33,11 +33,11 @@ module.exports = {
     },
     env_prod: {
        NODE_ENV: "prod",
-       watch: ["./server.js", "/etc/letsencrypt/live"]
+       watch: ["/etc/letsencrypt/live","./server.js","_helpers","users","vcgiMapData","vtInfo","vpMapped","vpPools","vpReview","vpSurvey","vpUtil","vpVisit"],
     },
     env_production: {
        NODE_ENV: "prod",
-       watch: ["./server.js", "/etc/letsencrypt/live"]
+       watch: ["/etc/letsencrypt/live","./server.js","_helpers","users","vcgiMapData","vtInfo","vpMapped","vpPools","vpReview","vpSurvey","vpUtil","vpVisit"],
     }
   }]
 }
