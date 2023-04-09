1319ef79d783b64ec40018fedae172f05d0e6b10
jloomisVCE <jloomis@vtecostudies.org>
2023-04-09 18:48:31 -0400

    restore route result

--- vpVisit/vpVisit.routes.js
+++ vpVisit/vpVisit.routes.js
@@ -107,8 +107,8 @@ function getPage(req, res, next) {
 function getById(req, res, next) {
     service.getById(req.params.id)
         .then(item => {
-          //item ? res.json({'rowCount': item.rows.length, 'rows': item.rows}) : res.sendStatus(404);
-          item ? res.json(item.rows) : res.sendStatus(404);
+          item ? res.json({'rowCount': item.rows.length, 'rows': item.rows}) : res.sendStatus(404);
+          //item ? res.json(item) : res.sendStatus(404);
         })
         .catch(err => next(err));
 }
